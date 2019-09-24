e2a518e238f3d28f23a47cbd8bfc7bfe5cb3ca99
Manville <rc0038309@student.riversidecollege.ac.uk>
2019-09-24 11:55:39 +0100

    Add project files.

---
+++ numbers/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            int i1 = 3 * 4;
+            int i2 = 12 / 4;
+            int i3 = 15 / 4;
+            int i4 = 15 % 4;
+            int i5 = 2 + 3 * 4;
+            int i6 = 2 + (3 * 4) / (2 - 4);
+            double d1 = 3.1 * 2.3;
+            double d2 = 1.0 / 3.0;
+            double d3 = 1 / 3;
+
+
+            richTextBox1.AppendText(i1 + "\n" + i2 + "\n" + i3 + "\n" + i4 + "\n" + i5 + "\n" + i6 + "\n");
+            richTextBox1.AppendText(d1 + "\n" + d2 + "\n" + d3 + "\n");
+
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
